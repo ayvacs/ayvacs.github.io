04a9b69c4b0e86b0f90a3beff4ee867381f09e17
dom (@frogweezer) <dompec07@gmail.com>
2021-12-30 15:30:35 -0500

    workingggg

---
+++ scripts/build.js
@@ -0,0 +1,25 @@
+$.getJSON("data/entries.json", function (data) {
+  //console.log(data);
+
+
+  /*
+  <div class="row-entry">
+    <div class="row">
+      <h3 class="column bullet-point">gba.js.org</h3>
+      <p class="column text-right">October 2021 - December 2021</p>
+    </div>
+
+    <p>A light-weight online emulator for the GameBoy Advance</p>
+
+    <a href="https://gba.js.org">Web link</a>
+    <a href="https://github.com/frogweezer/gba.js.org">GitHub source code</a>
+  </div>
+  */
+  var dataLength = data.length
+  for (var i = 0; i < dataLength; i++) {
+    var dataPoint = data[i];
+    console.log(dataPoint);
+
+    $("body").append("<div class=\"row-entry\"><div class=\"row\"><h3 class=\"column bullet-point\">" + dataPoint.name + "</h3></div></div>");
+  }
+});
