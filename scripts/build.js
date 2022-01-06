437969de0f6e9b0542d747a3553580eb4a9d0eb9
dom (@frogweezer) <dompec07@gmail.com>
2022-01-06 08:48:01 -0500

    Reformatting

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,14 @@
 $.get("data/entries.json", function (data) {
-  // Initialize Projects
-  var projects = data.projects;
-  var dataLength = projects.length;
-  var n = 0; // Counter variable
-  for (var i = 0; i < dataLength; i++) {
-    n++;
-    var dataPoint = projects[i];
+    // Initialize Projects
+    var projects = data.projects;
+    var dataLength = projects.length;
+    var n = 0; // Counter variable
+    for (var i = 0; i < dataLength; i++) {
+        n++;
+        var dataPoint = projects[i];
 
-    console.log("Built element " + n + "/" + dataLength);
-    $("#automagic-elements").append(`
+        console.log("Built element " + n + "/" + dataLength);
+        $("#automagic-elements").append(`
       <div class="row-entry">
         <div class="row">
           <h3 class="column bullet-point dataName">` + dataPoint.name + `</h3>
@@ -20,76 +20,76 @@ $.get("data/entries.json", function (data) {
       </div>
     `);
 
-    var rowEntry = document.getElementsByClassName("row-entry")[i];
-    var title = rowEntry.getElementsByClassName("column bullet-point dataName")[0];
+        var rowEntry = document.getElementsByClassName("row-entry")[i];
+        var title = rowEntry.getElementsByClassName("column bullet-point dataName")[0];
 
-    // Add icon image
+        // Add icon image
 
-    if (dataPoint.icon) {
-        title.innerHTML = `
+        if (dataPoint.icon) {
+            title.innerHTML = `
         <img style="position:absolute; margin-top:2px;" src="` + dataPoint.icon + `" height=18.72>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         ` + title.innerHTML;
-    };
+        };
 
-    // Add star icon
+        // Add star icon
 
-    if (dataPoint.isStarred) {
-        title.innerHTML = `
+        if (dataPoint.isStarred) {
+            title.innerHTML = `
         <img style="position:absolute; margin-top:2px;" src="assets/star.png" height=18.72>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         ` + title.innerHTML;
-    };
+        };
 
-    // Add languages
+        // Add languages
 
-    var langCount = Object.keys(dataPoint.languages).length;
-    if (langCount !== 0) {
-      var div = document.getElementsByClassName("entry-metadata")[i];
-      div.innerHTML += "<b>Languages:</b> ";
-      var langN = 0; // Counter variable
-      for (var l = 0; l < langCount; l++) {
-        langN++;
-        var lang = dataPoint.languages[l];
-        var div = document.getElementsByClassName("entry-metadata")[i];
+        var langCount = Object.keys(dataPoint.languages).length;
+        if (langCount !== 0) {
+            var div = document.getElementsByClassName("entry-metadata")[i];
+            div.innerHTML += "<b>Languages:</b> ";
+            var langN = 0; // Counter variable
+            for (var l = 0; l < langCount; l++) {
+                langN++;
+                var lang = dataPoint.languages[l];
+                var div = document.getElementsByClassName("entry-metadata")[i];
 
-        if (langN == langCount) {
-          div.innerHTML += lang;
-        } else {
-          div.innerHTML += lang + ", ";
+                if (langN == langCount) {
+                    div.innerHTML += lang;
+                } else {
+                    div.innerHTML += lang + ", ";
+                }
+            }
         }
-      }
-    }
 
-    // Add links
+        // Add links
 
-    var linkCount = Object.keys(dataPoint.links).length;
-    for (var key in dataPoint.links) {
-      var linkName = key;
-      var link = dataPoint.links[linkName];
+        var linkCount = Object.keys(dataPoint.links).length;
+        for (var key in dataPoint.links) {
+            var linkName = key;
+            var link = dataPoint.links[linkName];
 
-      var a = document.createElement("a");
-      a.innerHTML = linkName;
-      a.href = link;
-      a.target = "_blank"
+            var a = document.createElement("a");
+            a.innerHTML = linkName;
+            a.href = link;
+            a.target = "_blank"
 
-      rowEntry.append(a);
-    };
+            rowEntry.append(a);
+        };
 
-    // Add metadata
+        // Add metadata
 
-    var div = document.getElementsByClassName("entry-metadata")[i];
-    var isOpenSourced = dataPoint.isOpenSource;
-    if (isOpenSourced == "true") {
-      div.innerHTML += `
+        var div = document.getElementsByClassName("entry-metadata")[i];
+        var isOpenSourced = dataPoint.isOpenSource;
+        if (isOpenSourced == "true") {
+            div.innerHTML += `
       <br>
       &nbsp;
       <img style="position:absolute; margin-top:1px;" src="assets/github.png" height=17>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <b>Open-sourced</b>
       `;
-    } else if (isOpenSourced == "false") {
-      div.innerHTML += "<br><b>Closed-sourced</b>";
-    };
-  }
+        } else if (isOpenSourced == "false") {
+            div.innerHTML += "<br><b>Closed-sourced</b>";
+        };
+    }
 });
