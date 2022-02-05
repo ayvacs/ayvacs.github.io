56019c69d0799e2d6057a09279b8b30a410fbcf4
dom <dompec07@gmail.com>
2022-02-05 18:55:36 -0500

    kk

--- user-scripts/build.js
+++ user-scripts/build.js
@@ -54,7 +54,6 @@ $.get("data/entries.json", function (data) {
 
         if (dataPoint.headline) {
             metadata.innerHTML += `
-            <br>
             <i>` + dataPoint.headline + `</i><br><br>
             `;
         };
