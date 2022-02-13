f39271513da5cfe872192c6d0603355fb0476ff6
dom <dompec07@gmail.com>
2022-02-12 19:58:57 -0500

    okay!

--- user-scripts/build.js
+++ user-scripts/build.js
@@ -1,24 +1,45 @@
+// Functions
+function b (i) {
+    // Replaces i with the current Brigadier user count.
+    i = i.replace("{bDkUsers}", "1,500");
+    i = i.replace("{bUsers}", "6,000");
+
+    return i;
+};
+
+
 $.get("data/entries.json", function (data) {
     // Initialize Projects
     var projects = data.projects;
     var dataLength = projects.length;
+
     var n = 0; // Counter variable
     for (var i = 0; i < dataLength; i++) {
         n++;
         var dataPoint = projects[i];
 
         console.log("Built element " + n + "/" + dataLength);
-        $("#automagic-elements").append(`
-<div class="row-entry">
-<div class="row">
-<h3 class="column bullet-point dataName">` + dataPoint.name + `</h3>
-<p class="column text-right">` + dataPoint.date + `</p>
-</div>
 
-<div class="entry-metadata"></div>
-<p class="entry-description">` + dataPoint.description + `</p>
-</div>
-`);
+        var name = dataPoint.name;
+        var date = dataPoint.date;
+        var desc = dataPoint.description;
+
+        // Add nameDetails
+        if (dataPoint.nameDetails) {
+            name += "&nbsp;<span style=\"font-weight:normal;font-style:italic;font-size:0.8em\">" + dataPoint.nameDetails + "</span>"
+        };
+
+        $("#automagic-elements").append(`
+            <div class="row-entry">
+                <div class="row">
+                <h3 class="column bullet-point dataName">` + name + `</h3>
+                <p class="column text-right">` + date + `</p>
+            </div>
+
+            <div class="entry-metadata"></div>
+                <p class="entry-description">` + b(desc) + `</p>
+            </div>
+        `);
 
         var rowEntry = document.getElementsByClassName("row-entry")[i];
         var title = rowEntry.getElementsByClassName("column bullet-point dataName")[0];
@@ -29,10 +50,10 @@ $.get("data/entries.json", function (data) {
         // Ok first we gotta indent it
 
         if (dataPoint.isSub) {
-            rowEntry.style.position = "relative"
-            rowEntry.style.left = "2.5%"
-            rowEntry.style.width = "45%"
-        }
+            rowEntry.style.position = "relative";
+            rowEntry.style.left = "2.5%";
+            rowEntry.style.width = "45%";
+        };
 
         // Add icons
 
@@ -54,7 +75,7 @@ $.get("data/entries.json", function (data) {
 
         if (dataPoint.headline) {
             metadata.innerHTML += `
-            <i>` + dataPoint.headline + `</i><br><br>
+            <i>` + b(dataPoint.headline) + `</i><br><br>
             `;
         };
 
@@ -73,15 +94,15 @@ $.get("data/entries.json", function (data) {
                         metadata.innerHTML += lang;
                     } else {
                         metadata.innerHTML += lang + ", ";
-                    }
-                }
+                    };
+                };
             };
         };
 
         // Add platforms
 
         if (dataPoint.platforms) {
-            if (dataPoint.languages) { metadata.innerHTML += "<br>" }
+            if (dataPoint.languages) { metadata.innerHTML += "<br>" };
             var platformCount = Object.keys(dataPoint.platforms).length;
             if (platformCount !== 0) {
                 metadata.innerHTML += "<b>Platforms:</b> ";
@@ -94,8 +115,8 @@ $.get("data/entries.json", function (data) {
                         metadata.innerHTML += plat;
                     } else {
                         metadata.innerHTML += plat + ", ";
-                    }
-                }
+                    };
+                };
             };
         };
 
@@ -107,9 +128,9 @@ $.get("data/entries.json", function (data) {
                 var link = dataPoint.links[linkName];
 
                 var a = document.createElement("a");
-                a.innerHTML = linkName;
+                a.innerHTML = b(linkName);
                 a.href = link;
-                a.target = "_blank"
+                a.target = "_blank";
 
                 rowEntry.append(a);
             };
@@ -123,6 +144,11 @@ $.get("data/entries.json", function (data) {
             &nbsp;
             <img style="position:absolute; margin-top:1px;" src="assets/github.png" height=17>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <b>Open-sourced</b>
+            `;
+        } else if (dataPoint.isOpenSource) {
+            metadata.innerHTML += `
+            <br>
             <b>Open-sourced</b>
             `;
         } else {
